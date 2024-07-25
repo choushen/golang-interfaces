0d69044dc4cd5f849cc802589da969a0e5f2162f
choushen <jacob.mckenzie@icloud.com>
2024-07-25 22:44:00 +0100

    replaced client with a simple http.get

--- cmd/main.go
+++ cmd/main.go
@@ -21,10 +21,6 @@ func main() {
 	token := os.Getenv("TRELLO_API_TOKEN")
 	memberID := os.Getenv("TRELLO_MEMBER_ID")
 
-	fmt.Println("API_KEY: ", key)
-	fmt.Println("API_TOKEN: ", token)
-	fmt.Println("MEMBER_ID: ", memberID)
-
 	if key == "" || token == "" {
 		fmt.Println("API_KEY and API_TOKEN are required")
 		return
@@ -32,46 +28,28 @@ func main() {
 
 	url := fmt.Sprintf("https://api.trello.com/1/members/%s/boards?key=%s&token=%s&filter=open", memberID, key, token)
 
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error making request:", err)
+		fmt.Println(err)
 		return
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error: received status code %d\n", resp.StatusCode)
-		body, _ := io.ReadAll(io.Reader(resp.Body))
-		fmt.Printf("Response body: %s\n", body)
-		return
-	}
-
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error reading response body:", err)
+		fmt.Println(err)
 		return
 	}
-
-	// fmt.Println("Response body:", string(body))
 
 	var boards BoardList
-	if err := json.Unmarshal(body, &boards); err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
+	err = json.Unmarshal(body, &boards)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	// Print the parsed boards
-	fmt.Println("Boards:")
 	for _, board := range boards {
-		fmt.Printf("ID: %s, Name: %s\n", board.ID, board.Name)
+		fmt.Println(board.Name)
 	}
 
 }
