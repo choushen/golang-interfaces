1ae75d973afbeab4e973f85a5498508bc9c23872
Jacob <jacob.mckenzie@icloud.com>
2026-09-10 20:39:50 +0100

    chore: preserve local work

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,36 @@
 package main
 
-import (
-	"github.com/choushen/homeless-golang-code/cmd/rpg"
-)
-
 func main() {
-	// This is a comment
-	//fmt.Println("Hello, World!")
+
+	/*
+	*	Interfaces
+	 */
 
 	// Call the TrelloAPI function
 	//api.TrelloAPI()
 
-	orcCharacter := rpg.Orc{
-		Enemy: rpg.Enemy{
-			Health:      100,
-			AttackPower: 10,
-			Defense:     5,
-			Weapon:      "Axe",
-		},
-		Tribe: "Horde",
-	}
+	// orcCharacter := rpg.Orc{
+	// 	Enemy: rpg.Enemy{
+	// 		Health:      100,
+	// 		AttackPower: 10,
+	// 		Defense:     5,
+	// 		Weapon:      "Axe",
+	// 	},
+	// 	Tribe: "Horde",
+	// }
 
-	rpg.CounterAttack(&orcCharacter)
+	// rpg.CounterAttack(&orcCharacter)
 
-	goblinCharacter := rpg.Goblin{
-		Enemy: rpg.Enemy{
-			Health:      50,
-			AttackPower: 5,
-			Defense:     2,
-			Weapon:      "Dagger",
-		},
-		Homeland: "Forest",
-	}
+	// goblinCharacter := rpg.Goblin{
+	// 	Enemy: rpg.Enemy{
+	// 		Health:      50,
+	// 		AttackPower: 5,
+	// 		Defense:     2,
+	// 		Weapon:      "Dagger",
+	// 	},
+	// 	Homeland: "Forest",
+	// }
 
-	rpg.CounterAttack(&goblinCharacter)
+	// rpg.CounterAttack(&goblinCharacter)
+
 }
